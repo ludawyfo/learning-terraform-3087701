27f4b9b27c2565ce49c8061357948d1f15f8ee67
Ornel Franck WORA YENO <ornelfranck@hotmail.com>
2023-03-30 05:25:27 +0200

    correct solution

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -25,7 +25,6 @@ module "vpc" {
 
   enable_nat_gateway = true
 
-
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -33,9 +32,10 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                     = data.aws_ami.app_ami.id
+  instance_type           = var.instance_type
 
+  subnet_id               = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -48,8 +48,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
 
-  name    = "blog_sg"
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.vpc_id
+  name    = "blog"
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
