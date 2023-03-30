e53b01e5d68d779a909f74e01b884f90f67481dd
Ornel Franck WORA YENO <ornelfranck@hotmail.com>
2023-03-30 05:10:42 +0200

    trf version depreciated

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
@@ -46,7 +46,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.1"
+  version = "4.13.0"
 
   name    = "blog"
   vpc_id = module.vpc.public_subnets[0]
