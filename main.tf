deb2e4d0033314d691ad687dd9f54e8cd24da874
Ornel Franck WORA YENO <ornelfranck@hotmail.com>
2023-03-30 05:17:12 +0200

    error fix

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  name    = "blog"
+  name    = "blog_sg"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
